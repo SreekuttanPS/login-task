03a7a0d5d4ab442363c6f667d9a8f40bb80f7881
Sreekuttan <sreekuttan.workmail@gmail.com>
2025-07-08 09:39:43 +0530

    Update Input Component

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFormContext } from "react-hook-form";
+import OpenEye from "../../assets/svg/OpenEye";
+import ClosedEye from "../../assets/svg/ClosedEye";
 
 type InputProps = {
   name: string;
@@ -8,20 +10,32 @@ type InputProps = {
 };
 
 const Input: React.FC<InputProps> = ({ name, type = "text", placeholder }) => {
+  const [inputType, setInputType] = useState(type);
   const {
     register,
     formState: { errors },
   } = useFormContext();
 
+  const onEyeToggle = () => {
+    setInputType((prev) => (prev === "password" ? "text" : "password"));
+  };
+
   const error = errors[name]?.message as string | undefined;
   return (
     <div className="mb-4">
-      <input
-        type={type}
-        placeholder={placeholder || ""}
-        {...register(name)}
-        className="w-full px-4 py-2 bg-white/20 backdrop-blur-md rounded-md placeholder-white text-white focus:outline-none focus:ring-2 hover:ring-1 focus:ring-white cursor-default"
-      />
+      <div className={type === "password" ? "flex" : ""}>
+        <input
+          type={inputType}
+          placeholder={placeholder || ""}
+          {...register(name)}
+          className="w-full px-4 py-2 bg-white/20 backdrop-blur-md rounded-md placeholder-white text-white focus:outline-none focus:ring-2 hover:ring-1 focus:ring-white cursor-default"
+        />
+        {type === "password" ? (
+          <button onClick={onEyeToggle} className="flex items-center justify-center cursor-pointer ms-2 rounded-md px-2 py-2 bg-white/20 backdrop-blur-md">
+            {inputType === "password" ? <ClosedEye /> : <OpenEye />}
+          </button>
+        ) : null}
+      </div>
       {error && <p className="mt-1 text-sm text-red-400">{error}</p>}
     </div>
   );
